d580ccfcb763810cd189b7537f7fe466e2ef91a9
Arturo Montiel <ticmontiel200596@gmail.com>
2025-04-10 02:35:38 -0600

    change bundle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "test.poke.api.ams.poke_app"
+        applicationId = "ams.api.pokemon.app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
